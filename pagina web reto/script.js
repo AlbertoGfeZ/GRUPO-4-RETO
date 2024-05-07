0d381b9add9ea06ff6a23dd60d44919ffe743311
hugo-colio <168015459+hugo-colio@users.noreply.github.com>
2024-05-07 18:01:51 +0200

    Add files via upload

---
+++ pagina web reto/script.js
@@ -0,0 +1,25 @@
+// MENU HAMBURGUESA
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburguesa = document.querySelector('.menu-hamburguesa input');
+    const menuNav = document.querySelector('.contenido-nav ul');
+
+    hamburguesa.addEventListener('click', () => {
+        hamburguesa.parentElement.classList.toggle('abierta');
+        menuNav.classList.toggle('mostrar');
+    });
+});
+  
+// huevo de pascua 1
+var audio = null;
+
+function playSound() {
+    if (audio !== null) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    audio = new Audio("https://www.myinstants.com/media/sounds/himno-real-madrid-5.mp3");
+    audio.play();
+}
+
+
